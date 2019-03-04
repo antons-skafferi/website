24d761c4150187c81ea7b85a291682b4c9cff157
Max Wågberg <max.wagberg@hotmail.com>
2019-03-04 16:27:49 +0100

    Create event branch

---
+++ src/java/bean/eventBean.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import entities.Event;
+import facade.EventFacade;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.List;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author Max Wågberg
+ */
+@Named(value = "eventBean")
+@SessionScoped
+public class eventBean implements Serializable {
+
+    private String event_id;
+    private String description;
+    private String image;
+    private String strDate;
+    private Date event_date;
+    private String event_title;
+    @EJB
+    private EventFacade eventFacade;
+
+    public String getStrDate() {
+        return strDate;
+    }
+
+    public void setStrDate(String strDate) {
+        this.strDate = strDate;
+    }
+    
+    public String getEvent_title() {
+        return event_title;
+    }
+
+    public void setEvent_title(String event_title) {
+        this.event_title = event_title;
+    }
+    
+    public String getEvent_id() {
+        return event_id;
+    }
+
+    public void setEvent_id(String event_id) {
+        this.event_id = event_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Date getEvent_date() {
+        return event_date;
+    }
+
+    public void setEvent_date(Date event_date) {
+        this.event_date = event_date;
+    }
+    
+    public List<Event> eventList(){
+        return eventFacade.findAll();
+    }
+    
+ 
+
+    public void addEventForm() throws ParseException{
+        Date d;
+        d = new Date(2019,11,03);
+        eventFacade.create(new Event(null,d , event_title));
+    }
+    
+}
--- src/java/entities/Event.java
+++ src/java/entities/Event.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package entities;
 
 import java.io.Serializable;
+import java.sql.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,13 +15,15 @@ import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
- * @author Tobias
+ * @author Max Wågberg
  */
 @Entity
 @Table(name = "event")
@@ -28,7 +32,9 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "Event.findAll", query = "SELECT e FROM Event e")
     , @NamedQuery(name = "Event.findByEventId", query = "SELECT e FROM Event e WHERE e.eventId = :eventId")
     , @NamedQuery(name = "Event.findByDescription", query = "SELECT e FROM Event e WHERE e.description = :description")
-    , @NamedQuery(name = "Event.findByImage", query = "SELECT e FROM Event e WHERE e.image = :image")})
+    , @NamedQuery(name = "Event.findByImage", query = "SELECT e FROM Event e WHERE e.image = :image")
+    , @NamedQuery(name = "Event.findByEventDate", query = "SELECT e FROM Event e WHERE e.eventDate = :eventDate")
+    , @NamedQuery(name = "Event.findByEventTitle", query = "SELECT e FROM Event e WHERE e.eventTitle = :eventTitle")})
 public class Event implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -43,6 +49,15 @@ public class Event implements Serializable {
     @Size(max = 255)
     @Column(name = "image")
     private String image;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "event_date")
+    private Date eventDate;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "event_title")
+    private String eventTitle;
 
     public Event() {
     }
@@ -50,6 +65,13 @@ public class Event implements Serializable {
     public Event(Integer eventId) {
         this.eventId = eventId;
     }
+
+    public Event(Integer eventId, Date eventDate, String eventTitle) {
+        this.eventId = eventId;
+        this.eventDate = eventDate;
+        this.eventTitle = eventTitle;
+    }
+    
 
     public Integer getEventId() {
         return eventId;
@@ -73,6 +95,22 @@ public class Event implements Serializable {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getEventTitle() {
+        return eventTitle;
+    }
+
+    public void setEventTitle(String eventTitle) {
+        this.eventTitle = eventTitle;
     }
 
     @Override
@@ -99,5 +137,5 @@ public class Event implements Serializable {
     public String toString() {
         return "entities.Event[ eventId=" + eventId + " ]";
     }
-    
+
 }
