090df9d082431e1a2c0b8f8dcc4f4124b63d646f
Gustav Beije <gustavbeije@gmail.com>
2019-03-21 10:38:12 +0100

    Improve what events that get chosen in the upcoming events and previously events

--- src/java/bean/core/EventBean.java
+++ src/java/bean/core/EventBean.java
@@ -11,7 +11,9 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 
@@ -23,7 +25,15 @@ public class EventBean implements Serializable {
     
     
     public List<Event> getUpcomingEvents(int count){
-        List<Event> events = eventFacade.findAll();       
+        List<Event> events = eventFacade.findAll();
+        Calendar now = Calendar.getInstance();
+        // Using -1 be sure to get current and coming events
+        now.set(Calendar.HOUR, -1);
+        now.set(Calendar.MINUTE, 0);
+        now.set(Calendar.SECOND, 0);
+        now.set(Calendar.MILLISECOND, 0);
+        Date currentDate = now.getTime();
+        events.removeIf(e -> e.getEventDate().before(currentDate));
         events.sort(Comparator.comparing(Event::getEventDate));
         
         if(count > events.size()){
@@ -34,13 +44,23 @@ public class EventBean implements Serializable {
     }
     
     public List<Event> getEventsAfter(int count){
-        List<Event> events = eventFacade.findAll();       
-        events.sort(Comparator.comparing(Event::getEventDate));
+        List<Event> events = eventFacade.findAll();
+        Calendar now = Calendar.getInstance();
+        // Using -1 to only get events that has passed
+        now.set(Calendar.HOUR, -1);
+        now.set(Calendar.MINUTE, 0);
+        now.set(Calendar.SECOND, 0);
+        now.set(Calendar.MILLISECOND, 0);
+        Date currentDate = now.getTime();
+        events.removeIf(e -> e.getEventDate().after(currentDate) ||
+                e.getEventDate().equals(e));
+        events.sort(Comparator.comparing(Event::getEventDate).reversed());
         
-        if(count >= events.size())
-            return new ArrayList<Event>();
+        if(count > events.size()){
+            count = events.size();
+        }
         
-        return events.subList(count, events.size());
+        return events.subList(0, count);
     }
 
 }
